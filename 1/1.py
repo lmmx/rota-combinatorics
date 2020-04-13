f5a6a954f76f3073a07f8ee6f56737482aa1e55f
Louis Maddox <lmmx@users.noreply.github.com>
2020-04-13 10:22:07 +0100

    Update 1.py

--- 1/1.py
+++ 1/1.py
@@ -1,13 +1,60 @@
 a = set([1,2,3])
-b = set([1,2,3,4,5])
+b = set([1,2,3,4,8,20])
+print("a:", a)
+print("b:", b)
 
 print(len(a), "U", len(b))
-print("=", len(a.union(b)))
+uab = a.union(b)
+print("=", len(uab))
 
 print(len(b), "-", len(a))
-print("=", len(a.intersection(b)))
+iab = a.intersection(b)
+print("=", len(iab))
 
+print("\nSize of union plus size of intersection")
 print(len(a), "+", len(b))
+print("===")
+print("sum of set sizes:")
+print(len(uab), "+", len(iab))
+
 print(len(a) + len(b))
 print("===")
-print(len(a.union(b)) + len(a.intersection(b)))
+print(len(uab) + len(iab))
+
+print("----------")
+
+print("valuation function v: mean")
+def v(a):
+	return sum(a)/len(a)
+	
+print(v(a), "U", v(b))
+print("=", v(uab))
+
+print(v(b), "-", v(a))
+iab = a.intersection(b)
+print("=", v(iab))
+
+print("\nSize of union plus size of intersection")
+print(v(a), "+", v(b))
+print("===")
+print("sum of set means:")
+print(v(uab), "+", v(iab))
+
+print("----------")
+
+print("valuation function v: double size")
+def v(a):
+	return len(a) * 2
+	
+print(v(a), "U", v(b))
+print("=", v(uab))
+
+print(v(b), "-", v(a))
+iab = a.intersection(b)
+print("=", v(iab))
+
+print("\nSize of union plus size of intersection")
+print(v(a), "+", v(b))
+print("===")
+print("sum of set means:")
+print(v(uab), "+", v(iab))
